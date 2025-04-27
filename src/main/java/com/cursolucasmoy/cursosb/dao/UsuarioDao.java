e30cad52696eb36a1a12edf856ae9fdbd4a19df2
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-04-27 20:19:34 +0200

    dependency: jpa

---
+++ src/main/java/com/cursolucasmoy/cursosb/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package com.cursolucasmoy.cursosb.dao;
+
+import com.cursolucasmoy.cursosb.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+    List<Usuario> getUsuarios();
+}
---
+++ src/main/java/com/cursolucasmoy/cursosb/dao/UsuarioDaoImp.java
@@ -0,0 +1,20 @@
+package com.cursolucasmoy.cursosb.dao;
+
+import com.cursolucasmoy.cursosb.models.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class UsuarioDaoImp implements UsuarioDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Override
+    public List<Usuario> getUsuarios() {
+        return null;
+    }
+}
