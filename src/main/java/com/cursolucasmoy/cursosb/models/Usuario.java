22a47d3556552ec9d3a8f3ae359490b6f93eadcb
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-04-28 11:41:30 +0200

    más anotaciones en usuario

--- src/main/java/com/cursolucasmoy/cursosb/models/Usuario.java
+++ src/main/java/com/cursolucasmoy/cursosb/models/Usuario.java
@@ -1,24 +1,39 @@
 package com.cursolucasmoy.cursosb.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Entity
-@Table(name = "usuarios")
+@Entity @Table(name = "usuarios")
+@ToString @EqualsAndHashCode
 public class Usuario {
 
+    @Id @Column(name = "id")
     @Getter @Setter
     private long id;
+
+    @Column(name = "nombre")
     @Getter @Setter
     private String nombre;
+
+    @Column(name = "apellidos")
     @Getter @Setter
     private String apellidos;
+
+    @Column(name = "email")
     @Getter @Setter
     private String email;
+
+    @Column(name = "telefono")
     @Getter @Setter
     private String telefono;
+
+    @Column(name = "password")
     @Getter @Setter
     private String password;
 }
